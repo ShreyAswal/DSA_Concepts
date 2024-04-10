0129816fe554e69afb26e00e5b88b75d440990b8
ShreyAswal <aswalshrey@gmail.com>
2024-04-10 15:22:12 +0530

    Initialized trees folder

---
+++ Trees/basicTrees.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// A collection of nodes from which one node is taken as the root node and the rest of the nodes are divided into disjoint subsets...
+// and each subset is a tree or a subtree.
+
+// Formula to know how many trees can be generated according to no of nodes = T(n) =  (2n^ C _n)/n+1; eg: T(5) = 2*5^ C _5/5+1 = ((10*9*8*7*6)/(5*4*3*2*1))/6 = 42;
+// Formula to calculate no of trees with maximum height = 2^n; eg: n=3 => 2^3 = 8; 
+class Trees{
+
+
+};
+
+int main(){
+
+};  
